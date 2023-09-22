59a7d1dde2d381e18a5cbdbe8eab1763298fff60
AndrejChoo <97045273+AndrejChoo@users.noreply.github.com>
2023-09-22 09:25:31 +0300

    Update avr_top.v

--- firmware/altera/avr_top.v
+++ firmware/altera/avr_top.v
@@ -1,6 +1,6 @@
 //`define ind7seg
 
-module avr_top(
+module avr_new(
 	input wire clk,
 	input wire rst,
 	//GPIO
@@ -50,6 +50,7 @@ assign uart_rx_prog = (rst)? 1'b1 : uart_rx;
 wire[15:0]PROGDATA,PROGADD;
 wire PROGCLK, PROGWE;
 
+
 //IN CLOCK DIVIDER /2 /4
 reg[22:0]div;
 always@(posedge clk) div <= div + 1;
@@ -57,16 +58,18 @@ always@(posedge clk) div <= div + 1;
 //Interconnections
 core cpu0(.clk(hclk),.rst(rst),.SREG(SREG),.IOCNT(IOCNT),.IODOUT(IODOUT),.IODIN(IODIN),.IOW(IOW),.IOR(IOR),.SP(SP),//
 			 .IRQ_REQ(IRQ_REQ),.IRQ_ADD(IRQ_ADD),
+			 .PROGDI(PROGDATA),.PROGADD(PROGADD),.PROG_CLK(PROGCLK),.PROG_WE(PROGWE),
 			 //DEBUG
 			 .PCNT(PCNT),.ONUM(ONUM));
+			 
+progger mpr(.clk(clk),.rst(rst),.rx(uart_rx_prog),.DOUT(PROGDATA),.PADD(PROGADD),.wren(PROGWE),.clock(PROGCLK));
 				
 
 io mio0(.clk(hclk),.rst(rst),.SREG(SREG),.IOCNT(IOCNT),.IODIN(IODOUT),.IODOUT(IODIN),.IOW(IOW),.IOR(IOR),
 			.PB(PB),.PC(PC),.PD(PD),.SP(SP),.uart_rx(uart_rx_io),.uart_tx(uart_tx),.mosi(mosi),.miso(miso),.sck(sck),
 			.sda(sda),.scl(scl),.t0(t0),
 			.IRQ_REQ(IRQ_REQ),.IRQ_ADD(IRQ_ADD));
-
-`ifdef ind7seg
+`ifdef ind7seg			
 din7seg m7s(.clk(clk),.RAZR(RAZR),.SEG(SEG),.I0(PCNT[3:0]),.I1(PCNT[7:4]),.I2(PCNT[11:8]),.I3(PCNT[15:12]),
 				.I6(ONUM[3:0]),.I7(ONUM[6:4]),.I4(SREG[3:0]),.I5(SREG[7:4]));
 `endif			
